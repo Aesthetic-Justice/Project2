896e41f286383c23fd65cb2c8b4fe6a0655a61d3
Katie Wilson <kmcwilson4@gmail.com>
2022-11-24 20:47:06 -0500

    server figured out

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const hbs = exphbs.create({});
 const sess = {
   secret: 'Super secret secret',
   cookie: {   
@@ -26,9 +26,10 @@ const sess = {
   }),
 };
 
+
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
