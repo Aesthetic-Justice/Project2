84fc08a3ded0b5e5eb2997c00c4e5bd53e010d56
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-11-28 17:28:42 -0500

    Users are now able to signup

--- controller/API/user-routes.js
+++ controller/API/user-routes.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/index-User');
 
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
-      username: req.body.username,
+      name: req.body.username,
       email: req.body.email,
       password: req.body.password,
     });
-
     req.session.save(() => {
       req.session.loggedIn = true;
 
