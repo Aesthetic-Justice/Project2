69dd09bf138cb4b0db6c1f6ce057e5f72c2ca61d
Katie Wilson <kmcwilson4@gmail.com>
2022-11-28 21:38:00 -0500

    added to the attraction input

--- controller/API/post-routes.js
+++ controller/API/post-routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
+const Attraction = require('../../models')
 
-router.post('/', async (req, res) => {
+router.post('addAttractions', async (req, res) => {
     try {
-        const dbPostData = await Post.create({
-            title: req.body.title,
+        const dbPostData = await Attraction.create({
+            name: req.body.name,
             description: req.body.description,
             location_type: req.body.location_type,
-            rating: req.body.rating
         });
         res.status(200).json(dbPostData);
     } catch (err) {
