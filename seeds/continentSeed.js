b304ffb36edfbb5a4e9d53f09e1baebfb212a2d3
Katie Wilson <kmcwilson4@gmail.com>
2022-11-28 20:43:58 -0500

    added to the seeds, routes and handlebars

--- seeds/continentSeed.js
+++ seeds/continentSeed.js
@@ -1,12 +1,12 @@
 const { Continent } = require ('../models');
 
 const continentData = [
-    {
+    {   id: 1,
         name: 'North America',
         filename: 'north-america.jpg'
     }, 
 
-    {
+    {   id: 2,
         name: 'Europe', 
         filename:'europe.jpg', 
         
