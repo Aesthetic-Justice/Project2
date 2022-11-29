1ab819b4e8f3812915efce4ad7499a974ddf51a1
Katie Wilson <kmcwilson4@gmail.com>
2022-11-29 18:22:44 -0500

    Fixed the add attraction

--- public/js/attractionInput.js
+++ public/js/attractionInput.js
@@ -1,28 +1,28 @@
 const submit = document.querySelector(".button")
 
-const addAttraction = async (event)=>{
+const addAttraction = async (event) => {
     event.preventDefault();
 
-    const cityName= document.querySelector('.city-name');
+    const cityName = document.getElementById('city-dropdown');
     const attractionName = document.querySelector('.attraction');
-    const attractionType= document.querySelector('.location-type');
+    const attractionType = document.querySelector('.location-type');
     const description = document.querySelector('.description');
-    
+   console.log(cityName.value);
 
-    if (cityName && attractionName && attractionType && description){
+    if (cityName && attractionName && attractionType && description) {
         const response = await fetch('../api/post/addAttraction', {
-            method: 'POST', 
+            method: 'POST',
             body: JSON.stringify({
-                city_id: cityName,
-                name: attractionName,
-                location_type: attractionType,
-                description: description
+                city_id: cityName.value,
+                name: attractionName.value,
+                location_type: attractionType.value,
+                description: description.value
             }),
-            headers: {"Content-Type": "application/json"}
+            headers: { "Content-Type": "application/json" }
         })
-        if(response.ok) {
+        if (response.ok) {
             alert("Your attraction has been successfully added")
-            document.location.replace("/homepage/")
+            document.location.replace("/")
         }
         else {
             alert("Failed to create attraction")
